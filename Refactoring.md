4b0d362e695a86325f2873ebff59e67f904c6a31
Nitin_TS <nitin.agrawal@src.tabsquare.com>
2023-06-27 16:47:45 +0530

    refactor: refactored the dpk file and updated the readme file and unit testcase for it

--- Refactoring.md
+++ Refactoring.md
@@ -9,3 +9,8 @@ You've been asked to refactor the function `deterministicPartitionKey` in [`dpk.
 You will be graded on the exhaustiveness and quality of your unit tests, the depth of your refactor, and the level of insight into your thought process provided by the written explanation.
 
 ## Your Explanation Here
+
+We should optional chaining whenever it is possible to avoid extra code and easy to read
+Plus it is always to use logical operator as it will reduce lot of if conditions
+We should not just use redudant let. ( here for canditate)
+Code must be readible, more line of code doesn't guarantee robustness always
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,33 @@
-const { deterministicPartitionKey } = require("./dpk");
+const deterministicPartitionKey = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
+  });
+
+  it("returns a string", () => {
+    const result = deterministicPartitionKey({});
+    expect(typeof result).toBe("string");
+  });
+
+  it("returns a partition key", () => {
+    const result = deterministicPartitionKey({ partitionKey: "0" });
+    expect(typeof result).toBe("string");
+  });
+
+  it("returns a different value for different input", () => {
+    const input1 = { foo: "bar" };
+    const input2 = { foo1: "bar1" };
+    const result1 = deterministicPartitionKey(input1);
+    const result2 = deterministicPartitionKey(input2);
+    expect(result1).not.toEqual(result2);
+  });
+
+  it("returns the same value for the same input", () => {
+    const input = { foo: "bar" };
+    const result1 = deterministicPartitionKey(input);
+    const result2 = deterministicPartitionKey(input);
+    expect(result1).toEqual(result2);
   });
 });
